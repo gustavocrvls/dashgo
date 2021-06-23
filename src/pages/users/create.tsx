c8da02aade27de03ec4ac2ca03724e60631c6213
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-06-23 17:57:44 -0300

    User React hook form on create user page

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -9,12 +9,48 @@ import {
   Button,
 } from "@chakra-ui/react";
 import Link from "next/link";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 
+type CreateUserFormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+};
+
+const createUserFormSchema = yup.object().shape({
+  name: yup.string().required("Nome obrigatório"),
+  email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
+  password: yup
+    .string()
+    .required("Senha obrigatória")
+    .min(6, "No mínimo 6 caracteres"),
+  password_confirmation: yup
+    .string()
+    .oneOf([null, yup.ref("password")], "As senhas precisam ser iguais"),
+});
+
 export default function UserList() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(createUserFormSchema),
+  });
+
+  const { errors } = formState;
+
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
+    values
+  ) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    console.log(values);
+  };
+
   return (
     <Box>
       <Header />
@@ -22,7 +58,14 @@ export default function UserList() {
       <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
+        <Box
+          as="form"
+          flex="1"
+          borderRadius={8}
+          bg="gray.800"
+          p={["6", "8"]}
+          onSubmit={handleSubmit(handleCreateUser)}
+        >
           <Heading size="lg" fontWeight="normal">
             Criar usuário
           </Heading>
@@ -31,16 +74,35 @@ export default function UserList() {
 
           <VStack spacing="8">
             <SimpleGrid minChildWidth={240} spacing={["6", "8"]} w="100%">
-              <Input name="name" label="Nome completo"></Input>
-              <Input name="email" label="E-mail" type="email"></Input>
+              <Input
+                name="name"
+                label="Nome completo"
+                error={errors.name}
+                {...register("name")}
+              ></Input>
+              <Input
+                name="email"
+                label="E-mail"
+                type="email"
+                error={errors.email}
+                {...register("email")}
+              ></Input>
             </SimpleGrid>
 
             <SimpleGrid minChildWidth={240} spacing={["6", "8"]} w="100%">
-              <Input name="password" label="Senha" type="password"></Input>
+              <Input
+                name="password"
+                label="Senha"
+                type="password"
+                error={errors.password}
+                {...register("password")}
+              ></Input>
               <Input
                 name="password"
                 label="Confirmação da senha"
                 type="password"
+                error={errors.password_confirmation}
+                {...register("password_confirmation")}
               ></Input>
             </SimpleGrid>
           </VStack>
@@ -50,7 +112,9 @@ export default function UserList() {
               <Link href="/users" passHref>
                 <Button colorScheme="whiteAlpha">Cancelar</Button>
               </Link>
-              <Button colorScheme="pink">Salvar</Button>
+              <Button type="submit" colorScheme="pink" isLoading={formState.isSubmitting}>
+                Salvar
+              </Button>
             </HStack>
           </Flex>
         </Box>
