2acbf511285d482fcba744aa7054fcbb7e860538
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-06-23 17:48:11 -0300

    Integration with yup

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,29 +1,50 @@
-import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from '@chakra-ui/react';
+import { forwardRef, ForwardRefRenderFunction } from "react";
+import {
+  FormControl,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+  FormErrorMessage
+} from "@chakra-ui/react";
+import { FieldError } from "react-hook-form";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, error = null, ...rest },
+  ref
+) => {
+  console.log(error)
+
   return (
-    <FormControl>
-    {
-      !!label && <FormLabel htmlFor="email">{label}</FormLabel>
-    }
-    <ChakraInput
-      id={name}
-      name={name}
-      focusBorderColor="pink.500"
-      bg="gray.900"
-      variant="filled"
-      size="lg"
-      _hover={{
-        bgColor: 'gray.900'
-      }}
-      {...rest}
-    />
-  </FormControl>
+    <FormControl isInvalid={!!error}>
+      {!!label && (
+        <FormLabel htmlFor={name} id={`${name}-label`}>
+          {label}
+        </FormLabel>
+      )}
 
-  )
-}
+      <ChakraInput
+        name={name}
+        id={name}
+        focusBorderColor="pink.500"
+        bg="gray.900"
+        variant="filled"
+        size="lg"
+        _hover={{
+          bgColor: "gray.900",
+        }}
+        ref={ref}
+        {...rest}
+      />
+
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+    </FormControl>
+  );
+};
+
+export const Input = forwardRef(InputBase);
